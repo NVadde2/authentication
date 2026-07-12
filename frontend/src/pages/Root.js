ad5a9b65929997adab4277dffa1577379d8be35a
Neha Reddy <nehareddy@nehas-mbp.mynetworksettings.com>
2026-07-12 18:44:40 -0400

    Managing the Token Expiration

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,30 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useNavigation, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect (() => {
+    if (!token) {
+      return null;
+    }
+
+    if (token === 'EXPIRED') {
+      submit(null, { method: 'post', action: '/logout' });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { method: 'post', action: '/logout' });
+    }, 1 * 60 * 60 * 1000); // 1 hour
+  }, [token, submit]);
 
   return (
     <>
